5b77cc34b012769fea6faccf15a16465e873f909
slava.lobikov <lobikova12@mail.ru>
2023-10-18 15:59:04 +0300

    create admin-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {PostPageComponent} from "./post-page/post-page.component";
@@ -9,7 +9,10 @@ const routes: Routes = [
       {path: '', redirectTo:'/', pathMatch: 'full'},
       {path: '', component: HomePageComponent},
       {path: 'post/:id', component: PostPageComponent}
-    ]}
+    ]},
+  {
+    path: 'admin', loadChildren:() => import('./admin/admin.module').then(m => m.AdminModule)
+  }
 ];
 
 @NgModule({
